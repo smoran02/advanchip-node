d20bc9b6062e42fc90c59e451c1f9dab8af0e9e7
Spencer Moran <smoran02@gmail.com>
2014-02-05 10:38:51 -0800

    file stuff

---
+++ models/gateway.js
@@ -0,0 +1,12 @@
+module.exports = function(mongoose) {
+	var Schema = mongoose.Schema;
+	var gatewaySchema = new Schema(
+		{
+			gatewayID: { type: String },
+			admins: { type: [String] },
+			users: { type: [String] },
+			hostname: { type: String },
+			lights: { type: [ { light_id: String, state: Boolean } ] }
+		});
+	mongoose.model('Gateway', gatewaySchema);
+}
